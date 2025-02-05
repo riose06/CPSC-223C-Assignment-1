4feba0b1e41a87e0be7abd0eb1a67a0408dbd770
Ethan Rios <eirios2006@gmail.com>
2025-02-05 11:10:24 -0800

    Create messages that tell the user to enter their numbers, as well as allow them to do so

--- main.c
+++ main.c
@@ -12,11 +12,27 @@ int main() {
     fgets(user_name, 25, stdin);
     printf("Please enter your title: (Director, Sargent, Ms, Chief, Engineer, Fishmonger, etc): ");
     fgets(user_title, 15, stdin);
-    printf("Thank you %s %s", user_title, user_name);
+    printf("Thank you %s%s", user_title, user_name);
 
-    //char vol1, vol2, vol3;
-    //char alc1, alc2, alc3;
+    char vol1[10], vol2[10], vol3[10];
+    char alc1[10], alc2[10], alc3[10];
 
     printf("Please enter the volume (ml) of the liquid in beaker #1: ");
+    fgets(vol1, 10, stdin);
+    printf("Please enter the percent of alcohol in beaker #1 as decimal fraction: ");
+    fgets(alc1, 10, stdin);
+
+    printf("Please enter the volume (ml) of the liquid in beaker #2: ");
+    fgets(vol2, 10, stdin);
+    printf("Please enter the percent of alcohol in beaker #2 as decimal fraction: ");
+    fgets(alc2, 10, stdin);
+
+    printf("Please enter the volume (ml) of the liquid in beaker #3: ");
+    fgets(vol3, 10, stdin);
+    printf("Please enter the percent of alcohol in beaker #3 as decimal fraction: ");
+    fgets(alc3, 10, stdin);
+
+    printf("Thank you for entering these data. The results are being computed.");
+    
 }
 
